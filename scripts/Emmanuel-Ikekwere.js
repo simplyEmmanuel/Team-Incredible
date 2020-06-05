6fac734a2f3c5e462ada1b4ec5ccf46e08c48b48
simplyEmmanuel <demanuel2010@gmail.com>
2020-06-05 19:17:31 +0100

    emmanuelikekwere-HNG-00209

---
+++ scripts/Emmanuel-Ikekwere.js
@@ -0,0 +1,21 @@
+var genId = "HNG-00209";
+var genName = "Emmanuel Ikekwere";
+var genLang = "JavaScript";
+var genEmail = "emmaikekwere@gmail.com";
+
+var result =
+  "Hello World, this is" +
+  " " +
+  genName +
+  " " +
+  "with HNGi7 ID " +
+  genId +
+  " " +
+  "and email " +
+  genEmail +
+  " using " +
+  genLang +
+  " " +
+  "for stage 2 task.";
+
+console.log(result);
